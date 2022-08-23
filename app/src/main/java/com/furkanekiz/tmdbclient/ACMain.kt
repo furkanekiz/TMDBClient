638b5c6c2a0a6207f92ea2f64059e8c65ec0f3d4
furkanekiz <furkanekiz.06@gmail.com>
2022-08-23 17:25:39 +0300

    initial commit

---
+++ app/src/main/java/com/furkanekiz/tmdbclient/ACMain.kt
@@ -0,0 +1,11 @@
+package com.furkanekiz.tmdbclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ac_main)
+    }
+}
